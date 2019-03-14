3ec392a4bd03849f8e7c3aec23f444d8241da475
陈帅 <1301491593@qq.com>
2019-03-14 18:30:37 +0800

    '123'

--- php/application/index/controller/Index.php
+++ php/application/index/controller/Index.php
@@ -21,33 +21,7 @@ class Index extends Frontend
 
 	public function index()
 	{
-
-
-		$lunbo  = Ad::where('title', Ad::LUN_BO)->select();
-		$huoban = Ad::where('title', Ad::YOU_LIAN)->select();
-		$minghua = Ad::where('title', Ad::MINGHUA_LUNBO)->select();
-		$create = Ad::where('title', Ad::CREATE_VIDEO)->select();
-		$aboutus = Ad::where('title', Ad::ABOUT_US)->find();
-		$appfeature = Ad::where('title', Ad::APP_FEATURE)->select();
-    $travel = Ad::where('title', Ad::TRAVEL)->find();
-    $anchor = Ad::where('title', Ad::ANCHOR)->find();
-		$this->assign([
-			'lunbo' => $lunbo,
-			'huoban' => $huoban,
-      'minghua' => $minghua,
-      'create' => $create,
-			'aboutus' => $aboutus,
-			'appfeature' => $appfeature,
-      'travel' => $travel,
-      'anchor' => $anchor,
-			'createJson' => Collection($create)->toJson()
-		]);
-//		if(is_mobile()) {
-//			return $this->view->fetch('mobile');
-//		} else {
-//			return $this->view->fetch('index');
-//		}
-    return $this->view->fetch('index');
+    echo '欢迎进入栖霞前台！！';
 	}
 
 
